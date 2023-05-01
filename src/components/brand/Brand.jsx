ce8217c26c7e86c107fff40d7ad0064ed3a99ea1
02arslon <arslonruziboyev@gmail.com>
2023-05-01 19:38:59 +0900

    changed

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import './brand.css'
+import React from "react";
+import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 const Brand = () => {
   return (
     <div>
       <h1>Brand</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Brand
+export default Brand;
