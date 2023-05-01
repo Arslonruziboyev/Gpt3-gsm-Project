b09b4dacbe734460d4f9f59bfdd5d1f6895c4707
02arslon <arslonruziboyev@gmail.com>
2023-05-01 19:41:03 +0900

    changed

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -4,8 +4,14 @@ import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 const Brand = () => {
   return (
-    <div>
-      <h1>Brand</h1>
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+        <img src={slack} alt="slack" />
+        <img src={atlassian} alt="atlassiane" />
+        <img src={dropbox} alt="dropbox" />
+        <img src={shopify} alt="shopify" />
+      </div>
     </div>
   );
 };
