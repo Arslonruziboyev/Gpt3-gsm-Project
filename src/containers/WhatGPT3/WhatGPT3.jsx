aa684d3403ed804d5100289a6e29ac71ecd879cd
02arslon <arslonruziboyev@gmail.com>
2023-05-02 10:53:20 +0900

    it is changed

--- src/containers/WhatGPT3/WhatGPT3.jsx
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './whatGPT3.css'
+import React from "react";
+import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
     <div>
       <h1>WhatGPT3</h1>
     </div>
-  )
-}
+  );
+};
 
-export default WhatGPT3
+export default WhatGPT3;
