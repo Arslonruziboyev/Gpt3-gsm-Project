102fefd74d3cfa0f5de41787223b48956d249406
02arslon <arslonruziboyev@gmail.com>
2023-05-07 18:17:05 +0900

    changed

--- src/containers/possiblity/Possiblity.jsx
+++ src/containers/possiblity/Possiblity.jsx
@@ -8,6 +8,19 @@ const Possiblity = () => {
       <div className="gpt3__possibility-image">
         <img src={possiblityImage} alt="Possibility" />
       </div>
+      <div className="gpt3__possibility-content">
+        <h1 className="gradient__text">
+          <h4>Request Early Access to Get Started</h4>
+          The possibilities are <br /> beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
   );
 };
