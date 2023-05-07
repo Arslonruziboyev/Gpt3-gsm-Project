80a8d1c86e27acfc32636b09d85c14fdc0331154
02arslon <arslonruziboyev@gmail.com>
2023-05-07 17:40:10 +0900

    it has changed

--- src/containers/features/Feature.jsx
+++ src/containers/features/Feature.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import "./features.css";
 
 const Features = () => (
-  <div>
-    <h1>Hello</h1>
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">
+        The Future is Now and You Just Need to Realize It. Step into Future
+        Today. & Make it Happen.
+      </h1>
+      <p>Request Early Access to Get Started</p>
+    </div>
   </div>
 );
 
