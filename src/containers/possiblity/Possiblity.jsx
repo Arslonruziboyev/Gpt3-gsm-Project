98aaecc7591f879486819a942755b23032e94911
02arslon <arslonruziboyev@gmail.com>
2023-05-07 18:07:50 +0900

    changed

--- src/containers/possiblity/Possiblity.jsx
+++ src/containers/possiblity/Possiblity.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import './possiblity.css'
+import React from "react";
+import "./possiblity.css";
+import possiblityImage from "../../assets/possibility.png";
 
 const Possiblity = () => {
   return (
-    <div>
-      <h1>Possiblity</h1>
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={possiblityImage} alt="Possibility" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Possiblity
+export default Possiblity;
