4b9ab85f9b437758c678d69c54b825d2f994db31
02arslon <arslonruziboyev@gmail.com>
2023-05-17 18:33:43 +0900

    changed

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import './blog.css'
+import React from "react";
+import "./blog.css";
+import Article from "../../components/article/Article";
 
 const Blog = () => {
   return (
     <div>
       <h1>Blog</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
